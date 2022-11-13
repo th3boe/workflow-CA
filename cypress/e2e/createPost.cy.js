01bbfc265e73d9d113813113c07f00abd49f6dbb
Boe <boe3am@gmail.com>
2022-11-13 11:47:06 +0100

    cypress test change

--- cypress/e2e/createPost.cy.js
+++ cypress/e2e/createPost.cy.js
@@ -3,21 +3,34 @@ describe('workflow CA: Create post', () => {
     cy.visit('/');
     cy.clearLocalStorage();
     cy.wait(1000);
-    cy.get('#registerForm > div.modal-footer > button')
-      .contains('Login')
-      .click();
+    // cy.get('#registerForm > div.modal-footer > button')
+    //   .contains('Login')
+    //   .click();
+    // cy.wait(500);
+    // cy.get(
+    //   '#loginForm > div.modal-body > div.form-floating > input[type=email]'
+    // )
+    //   .should('exist')
+    //   .type('boepizza@noroff.no');
+    // cy.get(
+    //   '#loginForm > div.modal-body > div.form-floating > input[type=password]'
+    // )
+    //   .should('exist')
+    //   .type('Pizza1234');
+    // cy.get('#loginForm > div.modal-footer > button').contains('Login').click();
+    cy.get('.btn-close:visible').click();
     cy.wait(500);
-    cy.get(
-      '#loginForm > div.modal-body > div.form-floating > input[type=email]'
-    )
+    //gets the login button in header
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(500);
+    cy.get("input[type='email']:visible")
       .should('exist')
-      .type('boepizza@noroff.no');
-    cy.get(
-      '#loginForm > div.modal-body > div.form-floating > input[type=password]'
-    )
+      .type(`boepizza@noroff.no`);
+    cy.get("input[type='password']:visible")
       .should('exist')
-      .type('Pizza1234');
-    cy.get('#loginForm > div.modal-footer > button').contains('Login').click();
+      .type(`Pizza1234{enter}`);
+    cy.wait(2000);
+    cy.url().should('include', 'profile');
   });
   it('Create a new post is WORKING', () => {
     cy.wait(1500);
